d34dd48f0fd87e5bfb10baeb234c2e3cc4c54989
EHSANJiSHAD <tanvirehsan60@gmail.com>
2022-05-17 11:40:47 +0600

    manage page added with item details

--- src/pages/Manage/Manage.js
+++ src/pages/Manage/Manage.js
@@ -1,13 +1,30 @@
 import './Manage.css'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const Manage = () => {
     const {id} = useParams();
+    const [bookItem,setBookItem] = useState([]);
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/book/${id}`)
+        .then(res=>res.json())
+        .then(data=>setBookItem(data))
+    },[])
     return (
-        <div>
-            <h2>MANAGE YOUR BOOKS: {id}</h2>
+        <div className='container book-details'>
+            <img className='w-50' src={bookItem.img} alt="" />
+            <h3 className='item-text fs-3 item-name'>{bookItem.name}</h3>
+            <small className='book-text id'>ID: {bookItem._id}</small>
+            <p className='book-text bg-shadow'>{bookItem.description}</p>
+            <h5 className='item-text'>{bookItem.price} tk</h5>
+            <h6 className='book-text'>SOLD: {bookItem.sold}</h6>
+            <h6 className='book-text'>SUPPLIER NAME: {bookItem.supplier_name}</h6>
+
+            <button className='btn'>DELIVERED</button>
+            
+
         </div>
     );
 };
