a3b2562c88853438ff32e7da2236691be04d9ca9
EHSANJiSHAD <tanvirehsan60@gmail.com>
2022-05-17 21:07:20 +0600

    restock option added

--- src/pages/Manage/Manage.js
+++ src/pages/Manage/Manage.js
@@ -1,8 +1,9 @@
 import './Manage.css'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import auth from '../../firebase.init';
 import { toast } from 'react-toastify';
+import { Button, Form } from 'react-bootstrap';
 
 const Manage = () => {
     const {id} = useParams();
@@ -14,21 +15,21 @@ const Manage = () => {
         .then(data=>setBookItem(data))
     },[bookItem])
 
-    ////////////////////ITEM QUANTITY INCREASE//////////////////
+    ////////////////////ITEM QUANTITY DECREASE//////////////////
     
     const manageItemQuantity = ()=>{
         
         console.log(bookItem.quantity);
-        const updatedQuantity = (bookItem.quantity)  - 1;
-        const newQuantity = {updatedQuantity};
-        console.log(updatedQuantity);
-        
+        const quantity = (bookItem.quantity)  - 1;
+        const updateQuantity = {quantity};
+        console.log(updateQuantity);
+        console.log(id);
         fetch(`http://localhost:5000/book/${id}`,{
             method:'PUT',
             headers:{
                 'content-type' : 'application/json'
             },
-            body: JSON.stringify(newQuantity)
+            body: JSON.stringify(updateQuantity)
         })
         .then(res=>res.json())
         .then(data=>{
@@ -37,7 +38,31 @@ const Manage = () => {
         })
         
     }
-    ////////////////////ITEM QUANTITY INCREASE//////////////////
+    ////////////////////ITEM QUANTITY DECREASE//////////////////
+
+    /////////////////////////RESTOCK///////////////////////////
+    const numberRef = useRef();
+
+    const handleRestock = event=>{
+        event.preventDefault();
+        const restockVal = parseInt(numberRef.current.value); 
+        const quantity = (bookItem.quantity) + restockVal;
+        const restock = {quantity};
+        console.log(id);
+        fetch(`http://localhost:5000/book/${id}`,{
+            method:'PUT',
+            headers:{
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(restock)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log('processing',data);
+            toast('QUANTITY UPDATED');
+        })
+    }
+    /////////////////////////RESTOCK///////////////////////////
     return (
         <div className='container book-details'>
             <img className='w-50' src={bookItem.img} alt="" />
@@ -48,8 +73,20 @@ const Manage = () => {
             <h5 className='item-text'>{bookItem.price} tk</h5>
             <h6 className='book-text'>SOLD: {bookItem.sold}</h6>
             <h6 className='book-text'>SUPPLIER NAME: {bookItem.supplier_name}</h6>
+            <button onClick={manageItemQuantity} className='btn mb-5'>DELIVERED</button>
+            {/* RESTOCK OPTION */}
+            <Form className='form-div' onSubmit={handleRestock}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control  ref={numberRef} type="number" placeholder="ENTER RESTOCK AMOUNT" required/>
+                </Form.Group>
+                <Button className='mb-5 mt-0' variant="btn" type="submit">
+                    RESTOCK
+                </Button>
+                
+            </Form>
+            {/* RESTOCK OPTION */}
 
-            <button onClick={manageItemQuantity} className='btn'>DELIVERED</button>
+            
             
 
         </div>
